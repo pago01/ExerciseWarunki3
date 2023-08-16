6157e83acdd25c69a88d1dc76cdc3a27a061fe85
ACTFYZZ <ACTFYZZ@PL10006677>
2023-08-16 16:24:10 +0200

    Add project files.

---
+++ ExerciseWarunki3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Design;
+
+namespace ExerciseWarunki3
+{
+    //Data structure declaration
+
+    class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            int a = 0;
+
+            //a
+            Console.WriteLine("Podaj liczbe");
+            Int32.TryParse(Console.ReadLine(), out a);
+
+            //Display data
+            if (a > 0)
+            {
+                Console.WriteLine($"Liczba : {a} jest dodatnia");
+            }
+            else if (a < 0)
+            {
+                Console.WriteLine($"Liczba : {a} jest ujemna");
+            }
+            else
+                Console.WriteLine($"Liczba : {a} jest zerem");
+        }
+    }
+}
